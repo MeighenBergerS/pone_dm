14.11.21 the signal calculation is higher than literature not fixed yet

pone_dm/config.py
@@ -27,8 +27,8 @@ _baseconfig = {
         "year": [1, 2, 3, 4, 5, 6, 7, 8, 9]
     },
     "simulation parameters": {
-        "mass grid": np.logspace(3, 6, 20),
-        "sv grid": np.logspace(-26, -23, 20),
+        "mass grid": np.logspace(1, 4, 20),
+        "sv grid": np.logspace(-24, -21, 20),
         "uptime": 5 * 365 * 24 * 60 * 60,
         "low enery cutoff": 10e3,  # GeV
         "DM type k": 2
@@ -49,7 +49,7 @@ _baseconfig = {
         # Primary model
         'primary model': 'H4a',
         # Angles of interest currently not custom
-        'theta angles': np.array([0., 5., 10., 20., 30., 45., 60., 70., 90.]),
+        'theta angles': range(0, 91, 1),
         # Particles of interest
         'particles of interest': ['numu', 'nue', 'nutau']
     },
@@ -57,8 +57,7 @@ _baseconfig = {
     # P-ONE
     ###########################################################################
     "pone": {
-            'aeff location': '/home/kruteesh/Desktop/DM_nu_simulation_P-One/' +
-            'PONE_git/pone_work/data/',
+            'aeff location': '../data/',
             "specific particle scaling": {
                 "numu": 1.,
                 "nue": 1.,
@@ -73,7 +72,7 @@ _baseconfig = {
         "construction grid _d": np.logspace(-13, 16, 600),
         "_d storage": "../data/",
         "atmospheric storage": "../data/",
-        "scaling correction": 1
+        "scaling correction": 50
 
     }
 }

pone_dm/detectors.py
@@ -12,7 +12,7 @@ from tqdm import tqdm
 import csv
 
 from scipy.interpolate import UnivariateSpline
-_log = logging.getLogger(__name__)
+_log = logging.getLogger("pone_dm")
 
 
 class Detector(object):
@@ -172,6 +172,7 @@ class Detector(object):
         ----------------
         _bkgrd : dict [label : neutrino flavour]
                 [Total background ( atmos + astro )]  sumed over all thetas
+        _eff_are : np.array
         """
         # Converts simulation data to detector data
         # TODO: The flux data for signal  would be an array whereas here we
@@ -182,7 +183,7 @@ class Detector(object):
                 _flux[theta] = flux
         else:
             _flux = flux
-        at_counts_unsm, as_counts_unsm, = self._aeff.effective_area_func(
+        at_counts_unsm, as_counts_unsm, effe_area = self._aeff.effective_area_func(
             _flux, year)
         log_egrid = np.log10(self._egrid)
         self._atmos_counts = {}
@@ -226,7 +227,7 @@ class Detector(object):
             # Assuming the same background for all flavours ----------
             self._bkgrd[i] = self._tmp_bkgrd
 
-        return self._bkgrd
+        return self._bkgrd, effe_area
 
 # ------------------------------------------------
 # POne funcions -------- ------ -----
@@ -297,7 +298,6 @@ class Detector(object):
                  self.astro_flux()) * self._uptime *
                 self._ewidth * self._aeff.spl_A51(self._egrid)
             )
-            self._bkgrd[i] = self._bkgrd[i] * 46  # The scaling factor
-            print(i)
+            self._bkgrd[i] = self._bkgrd[i] / 1  # The scaling factor
 
         return self._bkgrd

pone_dm/dm2nu.py
@@ -34,18 +34,24 @@ class DM2Nu(object):
 
     def galactic_flux(self, E: np.array,
                       m_x: float, sv: float,
-                      k: float, J: float) -> np.array:
+                      k: float, J: float):
         """ Fetches the galactic flux
-        Add description
+        E : Energy Grid
+        m_x : Dark Matter mass
+        sv : signma_nu 
+        k : k factor (majorana: 2 otherwise 4)
+        J : J-factor
         """
         return self._dphi_dE_g(
             sv, k, m_x, E, J
         )
 
     def extra_galactic_flux(self, E: np.array,
-                            m_x: float, sv: float) -> np.array:
+                            m_x: float, sv: float):
         """ Fetches the extra-galactic flux
-        Add description
+        E : Energy grid
+        m_x : mass of Dark Matter
+        sv : sigma_nu
         """
         return self._dphide_lopez(
             E, m_x, sv
@@ -393,8 +399,7 @@ class DM2Nu(object):
         return aa * bb
 
     def _dphide_lopez(self, E: np.array, m_x: float, snu: float):
-        """ 
-        returns 
+        """ returns
         dphi/dE_Lopez : numpy array
         """
         z = m_x / E - 1

pone_dm/limit_calc.py
@@ -13,7 +13,7 @@ from atm_shower import Atm_Shower
 
 from scipy.stats import chi2
 from bkgrd_calc import Background
-_log = logging.getLogger(__name__)
+_log = logging.getLogger("pone_dm")
 
 
 class Limits(object):
@@ -68,8 +68,8 @@ class Limits(object):
         y = {}
         # for more generations adding the loop ----
         self._limit_grid = np.array([[
-                  ((self._signal(self._egrid, mass,
-                    sv)**2.
+                  (((self._signal(self._egrid, mass,
+                    sv))**2.
                     )[self._t_d:])
                   for mass in mass_grid]
                  for sv in sv_grid]
@@ -77,10 +77,12 @@ class Limits(object):
         for i in tqdm(config['atmospheric showers']['particles of interest']):
 
             _log.info('Starting the limit calculation for IceCube detector')
-            y[i] = np.array([[chi2.sf(np.sum(np.nan_to_num(x)), 2)
-                            for x in k/self._bkgrd[i][self._t_d:]]
+            y[i] = np.array([[chi2.sf(np.sum(
+                np.nan_to_num(x /
+                              self._bkgrd[i][self._t_d:])), 2)
+                            for x in k]
                              for k in self._limit_grid])
-        return y
+        return y, self._limit_grid
 
 # Limit calculation for Pone----------------------
 
@@ -119,10 +121,11 @@ class Limits(object):
                             ])
         for i in (config['atmospheric showers']['particles of interest']):
 
-            y[i] = np.array([[chi2.sf(np.sum(np.nan_to_num(x)), 2)
-                            for x in k/self._bkgrd[i][self._t_d:]]
+            y[i] = np.array([[chi2.sf(np.sum(np.nan_to_num(
+                x / self._bkgrd[i][self._t_d:])), 2)
+                            for x in k]
                              for k in self._limit_grid])
-        return y
+        return y, self._limit_grid
 
     def _find_nearest(self, array: np.array, value: float):
 

pone_dm/pone_aeff.py
@@ -11,7 +11,7 @@ from config import config
 from constants import pdm_constants
 from atm_shower import Atm_Shower
 import csv
-_log = logging.getLogger(__name__)
+_log = logging.getLogger("pone_dm")
 
 
 class Aeff(object):
@@ -74,7 +74,7 @@ class Aeff(object):
                     '../data/icecube_10year_ps/irfs/IC86_II_effectiveArea.csv',
                 ]
 
-            self.eff_dic = {
+            self._eff_dic = {
                         0: self.ice_parser(self.eff_areas[0]),
                         1: self.ice_parser(self.eff_areas[1]),
                         2: self.ice_parser(self.eff_areas[2]),
@@ -119,6 +119,9 @@ class Aeff(object):
                                                                 year])) *
                                              self.days)
 
+    def eff_dic(self):
+        return self._eff_dic
+
     @property
     def egrid(self):
         return self._egrid
@@ -147,10 +150,11 @@ class Aeff(object):
         # Apply the effective area to the simulation and return unsmeared
         # counts
 
-        ch_egrid = (self.eff_dic[year][:, 1] + self.eff_dic[year][:, 0])/2.
-        ch_theta = (self.eff_dic[year][:, 2] + self.eff_dic[year][:, 3])/2.
+        ch_egrid = (self._eff_dic[year][:, 1] + self._eff_dic[year][:, 0])/2.
+        ch_theta = (self._eff_dic[year][:, 2] + self._eff_dic[year][:, 3])/2.
         unsmeared_astro_counts = {}
         unsmeared_atmos_counts = {}
+        eff_area = {}
         for j, theta in enumerate(list(flux.keys())):
             surf_counts = flux[theta][-1]
 
@@ -167,9 +171,9 @@ class Aeff(object):
                     idE = np.abs(ch_egrid - loge).argmin()
                     all_near = (np.where(ch_egrid == ch_egrid[idE])[0])
                     idTheta = np.abs(ch_theta[all_near] - check_angle).argmin()
-                    tmp_eff.append(self.eff_dic[year][all_near, -1][idTheta])
+                    tmp_eff.append(self._eff_dic[year][all_near, -1][idTheta])
             loc_eff_area = np.array(tmp_eff)
-
+            eff_area[theta] = loc_eff_area
             tmp_at_un = ((surf_counts *
                           loc_eff_area *
                           self.uptime_tot_dic[year] *
@@ -183,7 +187,7 @@ class Aeff(object):
 
             unsmeared_atmos_counts[theta] = tmp_at_un
             unsmeared_astro_counts[theta] = tmp_as_un
-        return unsmeared_atmos_counts, unsmeared_astro_counts
+        return unsmeared_atmos_counts, unsmeared_astro_counts, eff_area
 
     @property
     def spl_A15(self):

pone_dm/signal_calc.py
@@ -11,7 +11,7 @@ from pone_aeff import Aeff
 from dm2nu import DM2Nu
 from constants import pdm_constants
 from detectors import Detector
-_log = logging.getLogger(__name__)
+_log = logging.getLogger("pone_dm")
 
 
 class Signal(object):
@@ -70,6 +70,8 @@ class Signal(object):
         -------
         total_new_counts : np.array
             The total new counts
+        totla_flux : np.array
+            the total_flux 
         """
         # Extra galactic
         _extra = self._dmnu.extra_galactic_flux(egrid, mass, sv)
@@ -85,14 +87,12 @@ class Signal(object):
         )
         # Converting fluxes into counts with effective area of IceCube !!!!
         #  These steps take a lot of time !!!!
-        _flux = self._detector.sim2dec(_ours, self._year)
-        _flux_ext = self._detector.sim2dec(_extra, self._year)
+        total_flux = _ours+_extra
+        total_new_counts = self._detector.sim2dec(total_flux, self._year)[0][
+            'numu'] / config["advanced"]["scaling correction"]
         # the sim_to_dec omits the dict but we assume
         # same for all neutrino flavours
-        total_new_counts = (
-            (_flux_ext["numu"] + _flux["numu"]) /
-            config["advanced"]["scaling correction"]
-        )
+
         return total_new_counts
 
     def _signal_calc_pone(self, egrid: np.array, mass: float,